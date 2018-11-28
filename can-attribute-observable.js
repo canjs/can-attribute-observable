ff5ab925890eeecc900541db1fd97e2e15bd2bcc
Matt Chaffe <matt@mattchaffe.uk>
2018-11-28 18:28:26 +0000

    Remove onValue and replace with onEmit when an event..
    ..is being used with AttributeObservable

--- can-attribute-observable.js
+++ can-attribute-observable.js
@@ -9,6 +9,7 @@ var canReflectDeps = require("can-reflect-dependencies");
 var ObservationRecorder = require("can-observation-recorder");
 var SettableObservable = require("can-simple-observable/settable/settable");
 var canAssign = require("can-assign");
+var canSymbol = require("can-symbol");
 
 // We register a namespaced radiochange event with the global
 // event registry so it does not interfere with user-defined events.
@@ -46,6 +47,16 @@ function AttributeObservable(el, prop, bindingData, event) {
 	this.prop = isMultipleSelect(el, prop) ? "values" : prop;
 	this.event = event || getEventName(el, prop, bindingData);
 	this.handler = this.handler.bind(this);
+
+	// If we have an event
+	// remove onValue/offValue and add onEvent
+	if (event) {
+		this[canSymbol.for('can.onValue')] = null;
+		this[canSymbol.for('can.offValue')] = null;
+		this[canSymbol.for('can.onEmit')] = AttributeObservable.prototype.on;
+		this[canSymbol.for('can.offEmit')] = AttributeObservable.prototype.off;
+	}
+
 
 	//!steal-remove-start
 	if(process.env.NODE_ENV !== 'production') {
@@ -102,7 +113,9 @@ canAssign(AttributeObservable.prototype, {
 		var queuesArgs = [];
 		this._value = attr.get(this.el, this.prop);
 
-		if (this._value !== old) {
+		// If we have an event then we want to enqueue on all changes
+		// otherwise only enquue when there are changes to the value
+		if (event || this._value !== old) {
 			//!steal-remove-start
 			if(process.env.NODE_ENV !== 'production') {
 				if (typeof this._log === "function") {
